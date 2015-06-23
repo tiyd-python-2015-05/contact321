201c5af81a222bdd1fe7a00e42d33c5947f22298
Clinton Dreisbach <clinton@dreisbach.us>
2015-06-23 10:35:41 -0400

    Added owner checking

--- contact321/contacts/views.py
+++ contact321/contacts/views.py
@@ -1,12 +1,14 @@
 from contacts.models import Contact
 from contacts.serializers import ContactSerializer
+from contacts.permissions import IsOwnerOrReadOnly
 from rest_framework import viewsets, permissions
 
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
