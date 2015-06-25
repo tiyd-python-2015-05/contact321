079cadd10f0004f689e9c3a288b7204a9d81c61b
Clinton Dreisbach <clinton@dreisbach.us>
2015-06-25 11:51:27 -0400

    Remove django debug toolbar

--- contact321/contact321/heroku_settings.py
+++ contact321/contact321/heroku_settings.py
@@ -4,6 +4,9 @@ import os
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
+BLACKLIST = ['debugtoolbar', 'django_extensions']
+INSTALLED_APPS = tuple([app for app in INSTALLED_APPS if app not in BLACKLIST])
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
